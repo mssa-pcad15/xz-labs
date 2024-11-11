e2e4c3be2f349f571ddaec1ede1569f0c6e4dcb4
Xian Zhou <natashazhou888@gmail.com>
2024-11-11 09:42:16 -0800

    Complete FtoC program that convers Fahrenheir to Celsuis

---
+++ week2/Week2Labs/FtoC/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FtoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fahrenheit = 94;
+
+            double celsius = (fahrenheit - 32) * (5 / 9f);
+
+            Console.WriteLine($"Farenheir is {fahrenheit}, conver to Celsius is {celsius:n2}");
+        }
+    }
+}
