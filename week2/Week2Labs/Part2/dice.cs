57f671cbcebd14842dfd3b8ba9ab1077ad698065
Xian Zhou <natashazhou888@gmail.com>
2024-11-12 11:11:13 -0800

    if else practice in C#

---
+++ week2/Week2Labs/Part2/dice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    internal class dice
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12);
+            int discountPercentage = 0;
+
+            // Your code goes here
+            if (daysUntilExpiration <= 0)
+                Console.WriteLine("Your subscription has expired.");
+            else if (daysUntilExpiration <= 1)
+                Console.WriteLine("Your subscription expires within a day!\r\nRenew now and save 20%!");
+            else if (daysUntilExpiration <= 5)
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\r\nRenew now and save 10%!");
+            else Console.WriteLine("Your subscription will expire soon. Renew now!");
+        }
+    }
+}
