64ed771e2a65c72155534cbbf5365a05960ecef9
Xian Zhou <natashazhou888@gmail.com>
2024-11-14 14:58:00 -0800

    HeroMonster Challenge

---
+++ week2/Week2Labs/Part3/WhileChallenge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3
+{
+    internal class WhileChallenge
+    {
+        internal static void HeroMonster()
+        {
+            int heroHealth = 10;
+            int monsterHealth = 10;
+            Random random = new Random();
+            int lostHealthMonster = random.Next(1, 11);
+            int lostHealthHero = random.Next(1, 11);
+
+            while (heroHealth > 0 && monsterHealth > 0)
+            {
+                monsterHealth -= lostHealthMonster;
+                heroHealth -= lostHealthHero;
+                Console.WriteLine($"Monster was damaged and lost {lostHealthMonster} health and now has {monsterHealth} health.");
+                Console.WriteLine($"Hero was damaged and lost {lostHealthHero} health and now has {heroHealth} health.");
+            }
+            if (heroHealth > monsterHealth)
+                Console.WriteLine("Hero wins!");
+            else
+                Console.WriteLine("Monster wins!");
+        }
+    }
+}
