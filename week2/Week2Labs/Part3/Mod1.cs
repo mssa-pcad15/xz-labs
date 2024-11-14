c31023f76c0c1e0712aaab316ea27f9bb69dd64b
Xian Zhou <natashazhou888@gmail.com>
2024-11-14 10:09:43 -0800

    code block challenge

---
+++ week2/Week2Labs/Part3/Mod1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3
+{
+    internal class Mod1
+    {
+        
+        internal static void Challenge()
+        {
+            Random coin = new Random();
+            int flip = coin.Next(0, 2);
+            Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+
+        }
+
+        internal static void Challenge2()
+        {
+            string permission = "Admin|Manager";
+            int level = 55;
+
+            if (permission.Contains("Admin") && level > 55) Console.WriteLine("Welcome, Super Admin user.");
+
+            if (permission.Contains("Admin") && level <= 55) Console.WriteLine("Welcome, Admin user.");
+
+            if (permission.Contains("Manager") && level >= 20) Console.WriteLine("Contact an Admin for access.");
+
+            if (permission.Contains("Manager") && level < 20) Console.WriteLine("You do not have sufficient privileges.");
+
+            if (!permission.Contains("Manager") && !permission.Contains("Admin")) Console.WriteLine("You do not have sufficient privileges.");
+
+
+
+
+
+        }
+    }
+}
---
+++ week2/Week2Labs/Part3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Part3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mod1.Unit1();
+            //Console.WriteLine($"Access static member: {VariableTest.StaticVar}");
+            Mod2.Challenge();
+        }
+    }
+}
