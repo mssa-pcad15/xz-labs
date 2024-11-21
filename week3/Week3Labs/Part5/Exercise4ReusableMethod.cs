5bbaab3a3109fabc401ec899add49b39823e8d5b
Xian Zhou <natashazhou888@gmail.com>
2024-11-21 08:21:44 -0800

    structure projects

---
+++ week3/Week3Labs/Part5/Exercise4ReusableMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part5
+{
+    internal class Exercise4ReusableMethod
+    {
+        internal static void ReusableMethod()
+        {
+            Random random = new Random();
+            int luck = random.Next(100);
+
+
+            string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
+            string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
+            string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
+            string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+            Console.WriteLine("A fortune teller whispers the following words:");
+            tellFortune();       
+
+            //a local method doesn't need to pass in local parameters it can access these local parameters by default
+            void tellFortune()
+            {
+                string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write($"{text[i]} {fortune[i]} ");
+                }
+            }
+        }
+    }
+}
---
+++ week3/Week3Labs/Part5/Mod1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part5
+{
+    internal class Mod1
+    {
+        internal static void Exercise1()
+        {
+            Console.WriteLine("Calling DisplayRandomNumber");
+            DisplayRandomNumber();
+        }
+
+        static void DisplayRandomNumber()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{random.Next(1, 100)} ");
+            }
+
+            Console.WriteLine();
+        }
+
+
+    }
+}
