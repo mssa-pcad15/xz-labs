b32355c9bcc1e68f2ab8b971448f1896644309a2
Xian Zhou <natashazhou888@gmail.com>
2024-11-23 11:35:45 -0800

    Challenge2 -- dice game

--- week3/Week3Labs/Part5/Challenge2DiceGame.cs
+++ week3/Week3Labs/Part5/Challenge2DiceGame.cs
@@ -1,44 +1,59 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Part5
-//{
-//    internal class Challenge2DiceGame
-//    {
-//        internal void DiceGame()
-//        {
-//            Random random = new Random();
+namespace Part5
+{
+    internal class Challenge2DiceGame
+    {
+        internal static void DiceGame()
+        {
+            Random random = new Random();
 
-//            Console.WriteLine("Would you like to play? (Y/N)");
-//            String? entry = Console.ReadLine();
-//            if (entry == "N") ;
-//            if (entry == "Y") PlayGame();
+            Console.WriteLine("Would you like to play? (Y/N)");
 
-//            if (ShouldPlay())
-//            {
-//                PlayGame();
-//            }
+            if (ShouldPlay())
+            {
+                PlayGame();
+            }
 
-//            void PlayGame()
-//            {
-//                var play = true;
+            // playGame()
+            void PlayGame()
+            {
+                var play = true;
 
-//                while (play)
-//                {
-//                    var target;
-//                    var roll;
+                while (play)
+                {
+                    
+                    var target = random.Next(1,6);
+                    var roll = random.Next(1,7);
 
-//                    Console.WriteLine($"Roll a number greater than {target} to win!");
-//                    Console.WriteLine($"You rolled a {roll}");
-//                    Console.WriteLine(WinOrLose());
-//                    Console.WriteLine("\nPlay again? (Y/N)");
+                    Console.WriteLine($"Roll a number greater than {target} to win!");
+                    Console.WriteLine($"You rolled a {roll}");
+                    Console.WriteLine(WinOrLose(target, roll));
+                    Console.WriteLine("\nPlay again? (Y/N)");
 
-//                    play = ShouldPlay();
-//                }
-//            }
-//        }
-//    }
-//}
+                    play = ShouldPlay();
+                }
+            }
+
+            // ShouldPlay()
+            bool ShouldPlay()
+            {
+                String? entry = Console.ReadLine();
+                if (entry == "Y" || entry == "y") return true;
+                return false;
+            }
+
+            // WinOrLose()
+            string WinOrLose(int target, int roll)
+            {
+                if (roll > target)
+                    return "You win";
+                else return "You lose";
+            }
+        }
+    }
+}
