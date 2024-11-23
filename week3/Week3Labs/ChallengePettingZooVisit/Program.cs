35aafe53be9ee4cc55c7ebbfa5c9268be5f9cfeb
Xian Zhou <natashazhou888@gmail.com>
2024-11-23 14:13:09 -0800

    Guided project -- petting zoo visit

---
+++ week3/Week3Labs/ChallengePettingZooVisit/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace ChallengePettingZooVisit
+{
+    internal class Program
+    {
+        /*
+         There are currently three visiting schools:
+
+            School A has six visiting groups (the default number)
+            School B has three visiting groups
+            School C has two visiting groups
+         
+        For each visiting school, perform the following tasks:
+
+            Randomize the animals
+            Assign the animals to the correct number of groups
+            Print the school name
+            Print the animal groups
+         
+         */
+
+        static void Main(string[] args)
+        {
+            string[] pettingZoo =
+            {
+                "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+                "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+                "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+            };
+
+            PlanSchoolVisit("School A");
+            PlanSchoolVisit("School B", 3);
+            PlanSchoolVisit("School C", 2);
+
+            void PlanSchoolVisit(string schoolName, int groups = 6)
+            {
+                RandomizeAnimals();
+                string[,] group1 = AssignGroup(groups);
+                Console.WriteLine(schoolName);
+                PrintAnimalGroup(group1);
+            }
+
+
+
+
+
+            //RandomizeAnimals()
+            void RandomizeAnimals()
+            {
+                Random random = new Random();
+
+                for (int i = 0; i < pettingZoo.Length; i++)
+                {
+                    int r = random.Next(i, pettingZoo.Length);
+
+                    string temp = pettingZoo[r];
+                    pettingZoo[r] = pettingZoo[i];
+                    pettingZoo[i] = temp;
+                }
+            }
+
+
+            //AssignGroup()
+            string[,] AssignGroup(int groups = 6)
+            {
+                string[,] result = new string[groups, pettingZoo.Length / groups];
+                int start = 0;
+
+                for (int i = 0; i < groups; i++)
+                {
+                    for (int j = 0; j < result.GetLength(1); j++)
+                    {
+                        result[i, j] = pettingZoo[start++];
+                    }
+                }
+
+                return result;
+            }
+
+
+
+
+            //PrintSchool()
+            Console.WriteLine("School A");
+
+          
+            // PrintAnimalGroup()
+            void PrintAnimalGroup(string[,] group)
+            {
+                for (int i = 0; i < group.GetLength(0); i++)
+                {
+                    Console.Write($"Group {i + 1}: ");
+                    for (int j = 0; j < group.GetLength(1); j++)
+                    {
+                        Console.Write($"{group[i, j]}  ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
