74d0d8ceccf1fe657587ce814f09e5f65e2ab5ce
Xian Zhou <natashazhou888@gmail.com>
2024-11-26 19:20:27 -0800

    Complete return correct result: AddTwoIntegersShouldReturnCorrectResult

---
+++ week4/Fundamentals1/Calculator1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals1
+{
+    public class Calculator1 //This Calculator needs to be public for the test file to see it
+    {
+        public static int Add(int op1, int op2)
+        {
+            return op1+op2;
+        }
+    }
+}
