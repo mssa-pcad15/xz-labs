72813dfc7f5aaf17ce1a7aeb02efe5453f41b3c5
Xian Zhou <natashazhou888@gmail.com>
2024-11-28 20:02:14 -0800

    Complete playing cards test ToString() method

---
+++ week4/FundamentalsTest1/PlayingCardsTest1.cs
@@ -0,0 +1,30 @@
+﻿using Fundamentals1.LearnTypes1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsTest1
+{
+    [TestClass]
+    public class PlayingCardsTest1
+    {
+        [TestMethod]
+        public void CardToStringPrintsUnicode()
+        {
+            //Arrange
+            Card aceOfSpade = new Card() { Rank=CardRank.Ace, Suit=CardSuit.Spade  };
+            string expect = "♠1"; //tested with ♠11, which is SpadeJack, passed the test
+
+            //Act
+            string actual = aceOfSpade.ToString();
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
+    }
+
+
+
+}
