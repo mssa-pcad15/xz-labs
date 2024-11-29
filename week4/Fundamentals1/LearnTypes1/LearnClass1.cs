37f9df500cdfcd9b391d6a78aa4c1d2172964904
Xian Zhou <natashazhou888@gmail.com>
2024-11-28 20:01:31 -0800

    Complete LearnClass test

---
+++ week4/Fundamentals1/LearnTypes1/LearnClass1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals1.LearnTypes1 //classes are organized under namespace, and the folder name also appears here
+{
+    public class LearnClass1
+    {
+        /*
+         * fields
+         */
+        //public fields, should use pascal 
+        public int InstanceMember;
+        public static int StaticMember;
+        //private fields, should use camelCasing with leading underscore
+        private DateTime _dob;
+        private string _password;
+
+
+        /*
+         * Properties
+         */
+        public int Age
+        {
+            get
+            {
+                return _dob.Year - DateTime.Now.Year;
+            }
+        }
+
+        public string Password
+        {
+            set //password can't be accessed, but it can be set
+            {
+                /*++++++++++ value is implicitly defined by the compiler when working with a property that has a set 
+                 * accessor in C#. The value keyword is a placeholder that represents the data being assigned to the
+                 * property. When you assign a value to the property, the compiler automatically passes that value 
+                 * into the set accessor using the value keyword.
+                +++++++++++*/
+                if (value.Length > 8 && value.Length < 15)
+                    _password = value;
+            }
+        }
+
+        /* 
+         * Method 
+         */
+        //Password is not allowed to be read, but we can verify the password
+        public bool VerifyPassword(string passwordToVerify)
+        {
+            return (this._password == passwordToVerify);
+        }
+
+        /* 
+         * constructor
+         */
+        //string password="":  means password is passed in with a default value, so when
+        //user created an obj of this LearnClass1, it is not mandatory to pass in this password
+        public LearnClass1(DateTime dob, string password="")
+        {
+            this._password = password;
+            this._dob = dob;    
+        }
+    }
+}
---
+++ week4/FundamentalsTest1/LearnClassTest1.cs
@@ -0,0 +1,69 @@
+﻿using Fundamentals1.LearnTypes1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsTest1
+{
+    [TestClass]
+    internal class LearnClassTest1
+    {
+        [TestMethod]
+        public void InstanceMemberExistsForEachInstance()
+        {
+            //Arrange
+            LearnClass1 obj1 = new LearnClass1(DateTime.Now);
+            LearnClass1 obj2 = new LearnClass1(DateTime.Now);
+
+            //Act
+            obj1.InstanceMember = 200;
+            obj2.InstanceMember = 300;
+
+            //Assert
+            Assert.IsTrue(obj1.InstanceMember != obj2.InstanceMember);
+            Assert.AreNotSame(obj1, obj2); //AreNotSame test if two objs points to the same location
+
+
+        }
+
+        [TestMethod]
+        public void EachInstanceHasTheirOwnData()
+        {
+            //Arrange, Act
+            LearnClass1 victor = new LearnClass1(new DateTime(1900, 5, 5), "password");
+            LearnClass1 bob = new LearnClass1(new DateTime(1970, 3, 5), "password2");
+            LearnClass1 tom = new LearnClass1(new DateTime(2012, 3, 5), "password3");
+            LearnClass1 anotherVictor = victor;
+
+            //Assert
+            Assert.AreNotSame(victor, bob);
+            Assert.AreNotSame(bob, tom);
+            Assert.AreSame(anotherVictor, victor);
+        }
+
+        [TestMethod]
+        public void ShowCorrectAgeGivenADob()
+        {
+            //Arrange
+            LearnClass1 victor = new LearnClass1(new DateTime(1900, 5, 5), "password");
+
+            //Act
+            int actual = victor.Age;
+            int expected = 34;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShowPasswordCanChange()
+        {
+            LearnClass1 victor = new LearnClass1(new DateTime(1900, 5,5), "password");
+            victor.Password = "newPassword";
+            Assert.IsTrue(victor.VerifyPassword("newPassword"));
+        }
+
+    }
+}
