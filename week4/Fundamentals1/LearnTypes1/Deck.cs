a0066a587b685893b420c2e8d704e8359b176a69
Xian Zhou <natashazhou888@gmail.com>
2024-11-29 09:47:37 -0800

    Complete "ADeckShouldAlwaysHave52Cards" test

---
+++ week4/Fundamentals1/LearnTypes1/Deck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals1.LearnTypes1
+{
+    public class Deck
+    {
+        public const int NumberOfCards = 52;
+        
+
+        
+    }
+}
