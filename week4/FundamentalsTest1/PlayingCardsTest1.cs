c41b8e81674888d67b4b861d9bec201d4c79b03f
Xian Zhou <natashazhou888@gmail.com>
2024-11-29 17:31:30 -0800

    Complete test: NewDeckOfCardWillHaveCardsInSuitToRankOrder()

--- week4/FundamentalsTest1/PlayingCardsTest1.cs
+++ week4/FundamentalsTest1/PlayingCardsTest1.cs
@@ -40,15 +40,30 @@ namespace FundamentalsTest1
         }
 
         [TestMethod]
-        public void NewDeckOfCardWillHaveCardsInSuitToRank()
-        {
+        //here the order refers to a deck of card starts from spade and ace, ends with Diamond and King
+        public void NewDeckOfCardWillHaveCardsInSuitToRankOrder() 
+        { 
+            //Idea:
+            // 1. declare private Card[] that has capacity to hold NumberOfCards
+            // 2. In the constructor of Deck class, use 2 loops, where outer loop enumerates Suit
+            //    and inner loop enumerates Rank to pupulate private array
+            // 3. Add a public read-only bool property called IsInNewDeckOrder
+            // 4. Create a method GetCard() returns a Card and accept int as index of array
+
             //Arrange
+            Deck newDeck = new Deck();
+            Card actualStart = newDeck.GetCardByIndex(0); //leaky implementation. "actual" value comes from the current program I am writing
+            Card expectStart = new Card() { Suit=CardSuit.Spade, Rank=CardRank.Ace };
 
+            Card actualEnd = newDeck.GetCardByIndex(51); //leaky implementation. 
+            Card expectEnd = new Card() { Suit = CardSuit.Diamond, Rank = CardRank.King };
 
-            //Act
-
+            //there is no Act
 
             //Assert
+            Assert.AreEqual(actualStart, expectStart); //Two variable of struct type is compared by value, hence
+                                                       //if two cards of the same suit and rank, they are equal.
+            Assert.AreEqual(actualEnd, expectEnd);
         }
 
         [TestMethod]
