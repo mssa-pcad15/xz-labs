64c94f833755127f8fa1299b5521583889c55de9
Xian Zhou <natashazhou888@gmail.com>
2024-12-01 06:09:38 -0800

    Class design tool explore. Start working on WarGameCards tests

---
+++ week4/Fundamentals1/LearnTypes1/WarGameCards.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fundamentals1.LearnTypes1
+{
+    public class WarGameCards
+    {
+        // | Constructor, to initialize 2 batches of 6 Decks shuffled
+        public WarGameCards()
+        {
+
+        }
+
+
+        //Make 1 Batch to be In-Play
+        //Make other Batch Stand-By
+
+        //Deal() method deals 1 card from the In-Play Batch
+
+        //Swap Batch at 90% cards dealt
+
+        //Reinitialize the now standby Batch
+    }
+}
---
+++ week4/FundamentalsTest1/WarGameCardsTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fundamentals1.LearnTypes1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals1.LearnTypes1.Tests
+{
+    [TestClass()]
+    public class WarGameCardsTests
+    {
+        [TestMethod()]
+        public void WarGameCardsInitializationTest()
+        {
+            WarGameCards shoe = new WarGameCards();
+            Assert.IsTrue(shoe.Batches.Length == 2);
+            Assert.IsTrue(shoe.Batches[0].Length == 6);
+            Assert.IsTrue(shoe.Batches[1].Length == 6);
+            Assert.AreSame(shoe.InPlayBatch, Batches[0]);
+            Assert.AreSame(shoe.StandbyBatch, Batches[1]);
+
+        }
+
+        [TestMethod()]
+        public void AfterDeal90PercentOfInPlayBatchSwapBatches()
+        {
+            WarGameCards shoe = new WarGameCards();
+            for (int i = 0; i < 0.9 * Deck.NumberOfCards * 6; i++)
+            {
+                _ = shoe.Deal(); //doesn't care the return value, only care about the side effect of Deal()
+            }
+            Assert.AreSame(shoe.InPlayBatch, Batches[1]);
+            Assert.AreSame(shoe.StandbyBatch, Batches[0]);
+
+            for (int i = 0; i < 0.9 * Deck.NumberOfCards * 6; i++)
+            {
+                _ = shoe.Deal(); //doesn't care the return value, only care about the side effect of Deal()
+            }
+            Assert.AreSame(shoe.InPlayBatch, Batches[0]);
+            Assert.AreSame(shoe.StandbyBatch, Batches[1]);
+
+        }
+}
