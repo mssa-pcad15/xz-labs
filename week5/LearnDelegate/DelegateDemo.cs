e58a52579832b6b546e08a47d0765beab041cdb1
Xian Zhou <natashazhou888@gmail.com>
2024-12-02 08:50:26 -0800

    Complete test : InstantiateADelegate()

---
+++ week5/LearnDelegate/DelegateDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnDelegate
+{
+    //Question: Whata things can be defined under namespace?
+    //
+    public class DelegateDemo
+    {
+        public double Multiply(double x, double y) => x * y;
+        public double Divide(double x, double y) => x / y;
+
+       
+    }
+
+    public delegate double MathOps(double a, double b);
+}
