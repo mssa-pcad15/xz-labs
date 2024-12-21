98708ced0cd2936c4e5060a4b9f8fc8c294caf1c
Xian Zhou <natashazhou888@gmail.com>
2024-12-21 10:07:27 -0800

    Complete DelegateDemoTest -- InstantiateADelegate(), InstantiateADelegateAndInvoke(), PassADelegateAsArgumentToMethod()

--- week5/LearnDelegate/DelegateDemo.cs
+++ week5/LearnDelegate/DelegateDemo.cs
@@ -10,11 +10,18 @@ namespace LearnDelegate
     //
     public class DelegateDemo
     {
-        public double Multiply(double x, double y) => x * y;
-        public double Divide(double x, double y) => x / y;
+        public double Multiply(double x, double y) => x * y; //this method fits delegate description, because it takes two
+                                                             //doubles and returns a double
+        public double Divide(double x, double y) => x / y; //this method fits delegate description, because it takes two
+                                                           //doubles and returns a double
 
-       
+
     }
 
+    /* 
+        A delegate named MathOps is declared, which can "reference" any method that:
+        - Takes two double parameters.
+        - Returns a double. 
+    */
     public delegate double MathOps(double a, double b);
 }
--- week5/LearnDelegateTest/DelegateDemoTest.cs
+++ week5/LearnDelegateTest/DelegateDemoTest.cs
@@ -11,21 +11,85 @@ namespace LearnDelegateTest
         {
             DelegateDemo d = new DelegateDemo(); //d is a variable of class, so points to an instance
             MathOps delegateInstance = d.Multiply; //delegateInstance is a  point to d.Multiply method
-            Assert.IsTrue(delegateInstance.Method.Name == "Multiply");
-            Assert.IsTrue(delegateInstance.Method.GetParameters().Length == 2);
+            Assert.IsTrue(delegateInstance.Method.Name == "Multiply");//delegate.Method represent the runtime method this instance points to
+            Assert.IsTrue(delegateInstance.Method.GetParameters().Length == 2); //two parameters that passed into "Multiply()" method
 
         }
 
         [TestMethod]
-        public void InstantiateADelegateAndInvoke() {
+        public void InstantiateADelegateAndInvoke()
+        {
             DelegateDemo d = new DelegateDemo();
-
             MathOps delegateInstance = d.Multiply;
 
+            /*###### 
+             * delegates have an Invoke() method that allows you to "explicitly" call the method(s) referenced by the 
+             * delegate. However, in most cases, you don't need to use Invoke() explicitly because delegates can be called 
+             * like regular methods.  
+             ###### */
             double result = delegateInstance.Invoke(5, 6);
             double expected = 30;
             Assert.AreEqual(expected, result);
-        
         }
+
+        [TestMethod]
+        public void PassADelegateAsArgumentToMethod()
+        {
+            DelegateDemo d = new DelegateDemo();
+            MathOps delegateInstance = d.Multiply;
+
+            double result = InvokeThis(delegateInstance); //We have passed this data into a method many times, now let's see pass instruction into a method
+            double expected = 30;
+            Assert.AreEqual(expected, result);
+        }
+
+        public double InvokeThis(MathOps instance) //this method invokes parameter delegate MathOps without knowing the implementing method, the only thing it knows it
+                                                   //will take two doubles and return a double
+        {
+            return instance.Invoke(5, 6);
+        }
+
+        //public void InvokeThis(VoidMathOps instance)
+        //{
+        //    instance.Invoke(5, 6);
+        //}
+
+        [TestMethod]
+        public void DelegateVariableSupportsMulticasting()
+        {
+            DelegateDemo d = new DelegateDemo();
+            /* "=" vs "+=": if use "=" on "delegateInstance = d.Divide;" then Divide will override Multiply */
+            MathOps delegateInstance = d.Multiply;
+            delegateInstance += d.Divide;  //I want delegate variable "delegateInstance" point to multiple things, so I need to piggyback using +=
+
+            double result = InvokeThis(delegateInstance);
+            //double expected = 30;
+            //Assert.AreEqual(expected, result); -- the last delegate variable will be invoked, which is "Divide"
+            Assert.AreEqual(result, 5.0 / 6);
+            Assert.IsTrue(delegateInstance.GetInvocationList().Length == 2);
+            Assert.IsTrue(delegateInstance.GetInvocationList()[0].Method.Name == nameof(d.Multiply));
+            Assert.IsTrue(delegateInstance.GetInvocationList()[1].Method.Name == nameof(d.Divide));
+        }
+
+        //[TestMethod]
+        ////public void MulticastDelegateShouldUseVoidReturnAndAccessResultsAsStateElseWhere() {
+        ////    DelegateDemo d = new DelegateDemo();
+        ////    VoidMathOps delegateInstance = d.VoidMultiply;
+        ////    delegateInstance += d.VoidDivide;
+
+        ////    InvokeThis(delegateInstance);
+
+
+
+        ////    Assert.IsTrue(d.Results.Contains(5.0 / 6));
+        ////    Assert.IsTrue(d.Results.Contains(5 * 6));
+        ////    Assert.IsTrue(delegateInstance.GetInvocationList().Length == 2);
+        ////    Assert.IsTrue(delegateInstance.GetInvocationList()[0].Method.Name == nameof(d.Multiply));
+        ////    Assert.IsTrue(delegateInstance.GetInvocationList()[1].Method.Name == nameof(d.Divide));
+
+        ////}
+
+
+
     }
 }
