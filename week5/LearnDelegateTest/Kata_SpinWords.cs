308da653fa28d5b3eeba11d02aa9c3679a1a4526
Xian Zhou <natashazhou888@gmail.com>
2024-12-25 15:14:06 -0800

    Kata SpinWords challenge, with Func Delegate

---
+++ week5/LearnDelegateTest/Kata_SpinWords.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearnDelegate;
+
+namespace LearnDelegateTest
+{
+    [TestClass]
+    public class Kata_SpinWords
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            string input = "Hey fellow warriors";
+            string expected = "Hey wollef sroirraw";
+
+            string actual = Kata.SpinWords(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            string input = "This is a test";
+            string expected = "This is a test";
+
+            string actual = Kata.SpinWords(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            string input = "This is another test";
+            string expected = "This is rehtona test";
+
+            string actual = Kata.SpinWords(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
