90b9ac1f5fbe0fcbe0487e02d0342ee2dc076398
Xian Zhou <natashazhou888@gmail.com>
2024-12-27 17:59:56 -0800

    LINQ exercise

--- week5/LearnDelegateTest/LinqTest.cs
+++ week5/LearnDelegateTest/LinqTest.cs
@@ -6,6 +6,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+
 
 namespace LearnDelegateTest
 {
@@ -17,6 +19,9 @@ namespace LearnDelegateTest
         {
             string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
 
+            // | refactor
+            var result = names.Where(s => s.Length == 4);
+            /*
             var result = names.Where((string s) =>
                 {
                     if (s.Length == 4) return true;
@@ -24,9 +29,117 @@ namespace LearnDelegateTest
 
                 }
             );
+            */
             Assert.IsTrue(result.Contains("Dick"));
             Assert.IsTrue(result.Contains("Mary"));
             Assert.IsFalse(result.Contains("Jay"));
+        }
+
+        [TestMethod]
+        public void LinqTestWhereWithIndex()
+        {
+            string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
+
+            var result = names.Where((s,i) => i<3);//"i" is an index
+            
+            Assert.IsTrue(result.Contains("Tom"));
+            Assert.IsTrue(result.Contains("Dick"));
+            Assert.IsTrue(result.Contains("Harry"));
+        }
+
+
+
+        [TestMethod]
+        public void LinqTestOrder()
+        {
+            string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
+
+            var result = names.OrderBy(s=>s.Length).ThenBy(s=>s[0]); //ThenBy further sort names in each group after "OrderBy". Here it further sort by each word's first letter
+
+            Assert.IsTrue(result.Last() == "Harry");
+            Assert.IsTrue(result.First() == "Jay");
+            Assert.AreNotEqual(names[4], result.Last());
+        }
+
+        [TestMethod]
+        public void TestTakeAndSkip()
+        {
+            //get all the classes from assembly
+            string[] typeNames = 
+                (from t in typeof(int).Assembly.GetTypes() select t.Name).ToArray();
+
+            var result = typeNames.Take(5);
+            Assert.AreEqual(5, result.Count());
+
+            var resultSkip = typeNames.Skip(5).Take(1);
+            var verify = typeNames.Take(6);
+            Assert.AreEqual(verify.Last(), resultSkip.First());
+        }
+
+        [TestMethod]
+        public void TestSelectInLinq()
+        {
+            var fonts = FontFamily.Families;
+            var result = fonts.Take(5).Select(f => f.Name); //gets the first 5 elements from the sequence
+            foreach(var name in result)
+            {
+                Debug.WriteLine(name);
+            }
+        }
+
+        // | select initial letter, name, length of name
+        [TestMethod]
+        public void TestSelectNewInLinq()
+        {
+            var fonts = FontFamily.Families;
+            var result = fonts.Take(5).Select
+                (
+                    //this new keyword creates a new type, this new type has 3 propertities
+                    f =>
+                    new
+                    {
+                        InitialLetter = f.Name[0],
+                        Name = f.Name,
+                        FontNameLength = f.Name.Length
+                    }
+                );
+#pragma warning restore CA1416 // Validate platform compatibility
+                              //gets the first 5 elements from the sequence
+                foreach(var thing in result) //must use "var" because anonymous class (line 85-90) doesn't have a name
+            {
+                Debug.WriteLine($"{thing.InitialLetter}, {thing.Name}, {thing.FontNameLength}");
+            }
+        }
+
+        [TestMethod]
+        public void TestGroupingInLinq()
+        {
+            var fonts = FontFamily.Families;
+            var resultDetail = fonts.Select
+                (
+                    //this new keyword creates a new type, this new type has 3 propertities
+                    f =>
+                    new
+                    {
+                        InitialLetter = f.Name[0],
+                        Name = f.Name,
+                        FontNameLength = f.Name.Length
+                    }
+                );
+#pragma warning restore CA1416 // Validate platform compatibility
+            var resultGrouped = resultDetail.GroupBy(anon => anon.InitialLetter); //group by anonbased on their initial letter
+            Debug.WriteLine($"There are {resultGrouped.Count()} groups from {resultDetail.Count()} fonts");
+
+            foreach (var thing in resultGrouped) // must use "var" because anonymous class (line 85-90) doesn't have a name
+                                                 // each thing in resultGrouped represent 1 group, keyed by initial letter
+            {
+                Debug.WriteLine($"Letter {thing.Key} has {thing.Count()} fonts."); //each thing is also an IEnumerable, so we can call Count(), thing.key is the distinct 
+                                                                                   //value with which the group is formed.
+                foreach(var font in thing)
+                {
+                    Debug.WriteLine($"\t{font.Name}");
+                }
+            }
         }
 
 
