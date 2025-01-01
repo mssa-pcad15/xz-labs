006e73554bb9a9acacb707130dd8c5e53cebf315
Xian Zhou <natashazhou888@gmail.com>
2025-01-01 10:19:58 -0800

    ArrayAreEqualInContent() extension method

---
+++ week6/DSA/DSATest1/Array.cs
@@ -0,0 +1,72 @@
+
+
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DSATest
+{
+    [TestClass]
+    public class ArrayTest
+    {
+        [TestMethod]
+        public void DeclareSingleDimensionArray()
+        {
+            int[] array = new int[5];
+            Assert.AreEqual(5, array.Length);
+
+            int[] array2 = [1,2,3,4,5]; //collection initializer
+            Assert.AreEqual(5, array2.Length);
+        }
+
+        [TestMethod]
+        public void DeclareMultiDimensionArray() //matrix
+        {
+            int[,] multiArray = new int[3, 2];
+            Assert.AreEqual(6, multiArray.Length); //Length property returns the total number of elements in the array.
+            Assert.AreEqual(3, multiArray.GetLength(0)); //GetLength(0) returns row number
+            Assert.AreEqual(2, multiArray.GetLength(1)); //GetLength(1) returns col number
+        }
+
+        [TestMethod]
+        public void InitializeMultiDimensionArrayWithInitializer()
+        {
+            int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } }; //array initializer
+            Assert.AreEqual(6, multiDimensionalArray2.Length);
+            Assert.AreEqual(2, multiDimensionalArray2.GetLength(0));
+            Assert.AreEqual(3, multiDimensionalArray2.GetLength(1));
+            Assert.AreEqual(2, multiDimensionalArray2.Rank); //Rank returns the number of dimensions of the multidimension array
+        }
+
+        [TestMethod]
+        public void MultiDimensionChallenge()
+        {
+            int[,] array1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; //array initializer
+            int[,] array2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; //array initializer
+            //Assert.AreEqual(array1, array2);
+            Assert.That.ArrayAreEqualInContent(array1, array2); //"That" is an instance of Assert, so that the extension method can extend it.
+                                                   //Because "Assert" is a class, a class can't receive an extension method, only an instance of it can
+                                                   //receive an extension method.
+                                                   //Hence if I call "Array.ArrayEqual" it will not work, it has to call the extension method on an instance
+
+            CollectionAssert.AreEqual(array1, array2); 
+            //how do we write an extension method to Assert class?
+            //How do we compare 2 array based on members
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ week6/DSA/DSATest1/AssertExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSATest
+{
+    internal static class AssertExtension
+    {
+        public static void ArrayAreEqualInContent(this Assert a, Array left, Array right)
+        {
+            //1. check if two arrays have same dimensions
+            if (left.Rank != right.Rank)
+                Assert.Fail("Arrays have different dimensions");
+            
+            for (int i = 0; i < left.Rank; i++)
+            {
+                if (left.GetLength(i) != right.GetLength(i))
+                    Assert.Fail($"Arrays have different number of elements at rank {i}");
+                
+                for (int j=0; j<left.GetLength(left.Rank-1); j++)
+                {
+                    if (!left.GetValue(i, j)!.Equals(right.GetValue(i, j))) //second ! is null-forgivable operator
+                        Assert.Fail($"Arrays contains different elements at index {i}, {j}");
+                }
+            
+            }
+        }
+    }
+}
